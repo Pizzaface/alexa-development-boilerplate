11c4e3c0523eb2b063fec1c7c32e1f31a4036b49
Jordan Pizza <Pizzaface@users.noreply.github.com>
2018-03-05 17:13:42 -0500

    Delete custom.js

--- lambda/custom.js
+++
@@ -1,9 +0,0 @@
-{
-  "name": "DecisionTree",
-  "version": "1.0.0",
-  "description": "Alexa Decision Tree template",
-  "main": "index.js",
-  "devDependencies": {},
-  "author": "Amazon.com",
-  "license": "SEE LICENSE IN license.md"
-}
