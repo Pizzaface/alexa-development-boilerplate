be5db66f92ac2bacd03861c93203c1591b2a3b9d
Jordan Pizza <Pizzaface@users.noreply.github.com>
2018-03-05 16:59:52 -0500

    Create custom.js

---
+++ lambda/custom.js
@@ -0,0 +1,9 @@
+{
+  "name": "DecisionTree",
+  "version": "1.0.0",
+  "description": "Alexa Decision Tree template",
+  "main": "index.js",
+  "devDependencies": {},
+  "author": "Amazon.com",
+  "license": "SEE LICENSE IN license.md"
+}
